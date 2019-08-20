dfb1e8ea43fd92a28b0754a0fbf4247623d7c422
Logan Born <lcborn4@gmail.com>
2019-08-20 09:31:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ amqp.connect(URL, function(error0, connection) {
     var exchange = "topic_logs";
     var args = process.argv.slice(2);
     var key = args.length > 0 ? args[0] : "test_rabbit";
-    var msg = message;
+    var msg = message.toString();
 
     channel.assertExchange(exchange, "topic", {
       durable: false
